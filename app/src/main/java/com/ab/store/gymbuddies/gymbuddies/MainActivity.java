04e4a3a9b6c58973c725f1e3e09426692d15de03
Aditya Sridhar <aditya123ss@gmail.com>
2016-10-15 17:17:36 -0400

    login page click button implemented

--- app/src/main/java/com/ab/store/gymbuddies/gymbuddies/MainActivity.java
+++ app/src/main/java/com/ab/store/gymbuddies/gymbuddies/MainActivity.java
@@ -5,11 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    void removeLoginPageElements() {
+        LinearLayout loginLayout = (LinearLayout) findViewById(R.id.login_page);
+        if (loginLayout != null) { loginLayout.setVisibility(View.GONE); }
+
+    }
+
+    View.OnClickListener btnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Log.d("MainActivity", "button clicked");
+            removeLoginPageElements();
+            // TODO: When button is clicked, remove contents of login view and add list adapter
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +35,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Button btn = (Button) findViewById(R.id.login_btn);
+        if (btn != null) {
+            btn.setOnClickListener(btnClickListener);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
